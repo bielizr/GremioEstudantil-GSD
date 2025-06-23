748e9fd75843e4f2e3fc6e1e6ceee6480254023f
bielizr <gabrielcallegari789@gmail.com>
2025-06-23 14:32:33 -0300

    Atualiza API_BASE_URL para Railway

--- gsd site/public/config.js
+++ gsd site/public/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = ""; // Será preenchido pela Railway ou em ambiente local
+const API_BASE_URL = "gremioestudantil-gsd-production.up.railway.app"; // Será preenchido pela Railway ou em ambiente local
 
 
